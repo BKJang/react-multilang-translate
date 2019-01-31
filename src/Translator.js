02b161a5538204cfd22504a1dbd1fb44141b77ff
jangbongki <jangbongki@futuresoft.co.kr>
2019-01-31 16:07:17 +0900

    Update Translator.js

--- src/Translator.js
+++ src/Translator.js
@@ -1,19 +1,24 @@
-import React, { Component } from 'react';
+import React, { PureComponent } from 'react';
 import { PropTypes } from 'prop-types';
 import Translate from './Translate';
 
-export default class Translator extends Component {
+export default class Translator extends PureComponent {
 
     constructor(props) {
         super(props);
         this.state = {
             translation: '',
+            msgObj: null
         };
     }
 
     componentDidMount() {
         const { transKey, locale, messageObj } = this.props;
         this.translate(transKey, locale, messageObj);
+    }
+
+    componentDidUpdate(prevProps) {
+        this.setState({ msgObj : prevProps.messageObj })
     }
 
     componentWillReceiveProps(nextProps) {
@@ -23,8 +28,11 @@ export default class Translator extends Component {
     }
 
     translate(transKey, locale, messageObj) {
-        if (transKey && locale)
-            this.setState({ translation: messageObj[locale][transKey] });
+        const { msgObj } = this.state;
+        const message = msgObj ? msgObj : messageObj;
+
+        if (transKey && locale && message)
+            this.setState({ translation: message[locale][transKey] });
     }
 
     render() {
