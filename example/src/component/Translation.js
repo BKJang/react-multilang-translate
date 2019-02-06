160420ffbc303816ba914ebdb303be6c0ccd0830
SeonHyungJo <seonhyung.jo@gmail.com>
2019-02-06 23:36:58 +0900

    Refactoring / Example dir

--- example/src/component/Translation.js
+++ example/src/component/Translation.js
@@ -1,27 +1,14 @@
-import React, { Component } from 'react';
+import React from 'react';
 import { Translator } from 'react-multilang-translate';
 import { MESSAGE } from './message';
-import { connect } from "react-redux";
 
-class Translation extends Component {
 
-    render() {
-        const { locale, transKey } = this.props;
-        const messageObj = {...MESSAGE}
-        return (
-            <Translator
-                transKey={transKey}
-                locale={locale}
-                messageObj={messageObj}
-            />
-        );
-    }
-}
+const Translation = ({ locale, transKey }) => (
+    <Translator
+        transKey={transKey}
+        locale={locale}
+        messageObj={MESSAGE}
+    />
+)
 
-const mapStateToProps = state => ({
-    locale: state.translator.locale
-});
-
-export default connect(
-    mapStateToProps,
-)(Translation);
+export default Translation
--- example/src/store/modules/index.js
+++
@@ -1,6 +0,0 @@
-import { combineReducers } from "redux";
-import translator from "./translator";
-
-export default combineReducers({
-    translator
-});
